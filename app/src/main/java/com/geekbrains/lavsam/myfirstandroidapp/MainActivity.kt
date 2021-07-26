2d173e3d1cf2b3e150f9280eed43b8f589be0b22
lavsam-dev <lavsam@gmail.com>
2021-07-26 18:55:25 +0400

    Преобразовали Java в Kotlin

--- app/src/main/java/com/geekbrains/lavsam/myfirstandroidapp/MainActivity.kt
+++ app/src/main/java/com/geekbrains/lavsam/myfirstandroidapp/MainActivity.kt
@@ -1,56 +1,38 @@
-package com.geekbrains.lavsam.myfirstandroidapp;
+package com.geekbrains.lavsam.myfirstandroidapp
 
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
-
-    private EditText firstDigit;
-    private EditText secondDigit;
-    private TextView textResult;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_digit_cheker);
-
-        firstDigit = findViewById(R.id.first_digit);
-        secondDigit = findViewById(R.id.second_digit);
-
-        Button compareButton = findViewById(R.id.btn_compare);
-
-        textResult = findViewById(R.id.text_result);
-
-        compareButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                compareDigits();
-            }
-        });
-
+class MainActivity : AppCompatActivity() {
+    private var firstDigit: EditText? = null
+    private var secondDigit: EditText? = null
+    private var textResult: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_digit_cheker)
+        firstDigit = findViewById(R.id.first_digit)
+        secondDigit = findViewById(R.id.second_digit)
+        val compareButton = findViewById<Button>(R.id.btn_compare)
+        textResult = findViewById(R.id.text_result)
+        compareButton.setOnClickListener { compareDigits() }
     }
 
-    private void compareDigits() {
+    private fun compareDigits() {
         try {
-            Integer firstValue = Integer.valueOf(firstDigit.getText().toString());
-
-            Integer secondValue = Integer.valueOf(secondDigit.getText().toString());
-
-            if (firstValue.equals(secondValue)) {
-                textResult.setText("Equal");
+            val firstValue = Integer.valueOf(firstDigit!!.text.toString())
+            val secondValue = Integer.valueOf(secondDigit!!.text.toString())
+            if (firstValue == secondValue) {
+                textResult!!.text = "Equal"
             } else {
-                textResult.setText("Not Equal");
+                textResult!!.text = "Not Equal"
             }
-        } catch (NumberFormatException exception) {
-            textResult.setText("Please enter a digit, damn ass!");
-            Log.v("Digit Checker", "User is minimally acceptional.");
+        } catch (exception: NumberFormatException) {
+            textResult!!.text = "Please enter a digit, damn ass!"
+            Log.v("Digit Checker", "User is minimally acceptional.")
         }
-
     }
 }
